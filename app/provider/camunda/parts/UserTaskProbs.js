7fca369838bd6810fc24b7f6066bc404faca5fae
dkassel <dani3l.ka@gmail.com>
2016-05-31 16:07:01 +0200

    Added CandidateGroups autocompletion

--- app/provider/camunda/parts/UserTaskProbs.js
+++ app/provider/camunda/parts/UserTaskProbs.js
@@ -65,7 +65,6 @@ module.exports = function (group, element) {
 
 
         // AUTOCOMPLETE INIT
-        // TODO:  Candidate Users, Candidate Groups, Priority
         $(function () {
             function split( val ) {
                 return val.split( /;\s*/ );
@@ -165,7 +164,69 @@ module.exports = function (group, element) {
                     }
                 });
 
+            //CANDIDATE GROUPS
+            var candidateGroupsSelektor = $("#camunda-candidateGroups");
+            candidateGroupsSelektor.bind( "keydown", function( event ) {
+                if ( event.keyCode === $.ui.keyCode.TAB &&
+                    $( this ).autocomplete( "instance" ).menu.active ) {
+                    event.preventDefault();
+                }
+            });
+            candidateGroupsSelektor.autocomplete({
+                minLength: 0,
+                source: function( request, response ) {
+                    var searchTerm = extractLast(request.term);
+                    console.log (searchTerm);
+                    $.ajax({
+                        url: "http://test-cdn.abas.de/hska/bpmn/teams?q=" + searchTerm,
+                        success: function (data) {
+                            var transformed = $.map(data.team_service.teams, function (el) {
+                                return {
+                                    label: el.name,
+                                    id: el.id
+                                };
+                            });
+                            response(transformed);
+                        },
+                        error: function () {
+                            response([]);
+                        }
+                    });
+                },
+                focus: function() {
+                    // prevent value inserted on focus
+                    return false;
+                },
+                select: function( event, ui ) {
+                    var terms = split( this.value );
+                    // remove the current input
+                    terms.pop();
+                    // add the selected item
+                    terms.push( ui.item.value );
+                    // add placeholder to get the comma-and-space at the end
+                    terms.push( "" );
+                    this.value = terms.join( "; " );
 
+                    var selected_label = ui.item.label;
+                    var selected_value = ui.item.value;
+
+                    var labels = $('#labels').val();
+                    var values = $('#values').val();
+
+                    if(labels == "")
+                    {
+                        $('#labels').val(selected_label);
+                        $('#values').val(selected_value);
+                    }
+                    else
+                    {
+                        $('#labels').val(labels+";"+selected_label);
+                        $('#values').val(values+";"+selected_value);
+                    }
+
+                    return false;
+                }
+            });
 
 
 
